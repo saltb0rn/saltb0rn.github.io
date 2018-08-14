f2858a20ee0212223767d9d7cdec6959d6b27c63
salt <salt@salt.salt-island>
2018-08-14 17:42:35 +0800

    Add mysql

--- drafts/setup.py
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/env python3
-
-from distutils.core import setup, Extension
-
-setup(name="Extest", ext_modules=[Extension("Extest", sources=['Extest.c'])])
---
+++ files/tmp/rename-files.sh
@@ -0,0 +1,1 @@
+/home/salt/Documents/code/shell/rename-files.sh
