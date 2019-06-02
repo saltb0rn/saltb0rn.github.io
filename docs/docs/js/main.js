f1255742f0aa75ee0e7fad3c04d23bd8f9818431
saltb0rn <asche34@outlook.com>
2019-06-02 15:55:27 +0800

    Recompile

---
+++ docs/docs/js/main.js
@@ -0,0 +1,103 @@
+window.document.addEventListener('DOMContentLoaded', function() {
+    /*
+      window.onload 方法是在整个页面加载完后才执行,包括样式文件和脚本文件;
+      而 DOMContentLoaded 事件是在文档对象加载完才执行,不包括样式文件和脚本文件,
+      只是元素加载完就可以.也就是说在脚本事件加载前可以阻止触发.用这事件可以在评论加载前移除它.
+    */
+
+    let ptype;
+    var re_index = /^(?:\/|\/index\.html(?:\/|\/.*)?)$/;
+    var re_post = /^\/(?:.*)\.html(?:\/|\.*)?$/;
+    // the behaviors of different type of pages are different.
+    if (re_index.test(this.location.pathname)) {
+        ptype = 'index';
+    } else if (re_post.test(this.location.pathname)) {
+        ptype = 'post';
+    } else {
+        ptype = 'other';
+    }
+
+    function setup_style_for_index() {
+        console.log("The type of page is", ptype);
+        var validation = document.querySelector(".validation");
+        if (validation) {
+            validation.style.display = "none";
+        }
+    }
+
+    function setup_style_for_post() {
+        console.log("The type of page is", ptype);
+        var post_abstract = document.querySelector(".abstract");
+        var toc = document.querySelector("#table-of-contents");
+        var comment = document.querySelector("#disqus_thread");
+        var comment_clone = comment.cloneNode(true);
+        if (toc) {
+            post_abstract.parentNode.insertBefore(post_abstract, toc);
+        }
+        // TODO: 加载页面时移除评论块,之后设计一个按钮,点击的时候再把comment_clone添加回去.
+        // if (comment) {
+        //     comment.style.display = "none";
+        //     comment.remove();
+        //     console.log(comment_clone);
+        // }
+    }
+
+    function setup_style_for_other() {
+        console.log("The type of page is", ptype);
+    }
+
+    var page_style_map = {
+        index: setup_style_for_index,
+        post: setup_style_for_post,
+        other: setup_style_for_other
+    };
+
+    function setup_style() {
+        var func = page_style_map[ptype];
+        if (func) {
+            func();
+        } else {
+            console.log("No such type of page");
+        }
+    }
+
+    function log_error(err, url='') {
+        if (!url) {
+            var div = (window.document.querySelector("div.errlogs") ||
+                       window.document.createElement('div'));
+            if (!("errlogs" in div.classList))
+            {
+                window.document.appendChild(div);
+                div.className = "errlogs";
+            }
+            var p = window.document.createElement('p');
+            p.className = 'err';
+            p.textContent = "message: "+ err.message + "\n" + "stack: " + er.stack + "\n";
+            div.appendChild(p);
+            return;
+        }
+        fetch(url, {
+            method: "POST",
+            mode: "no-cache",
+            credentials: "same-origin",
+            headers: {
+                "Content-Type": "application/json; charset=utf-8"
+            },
+            redirect: "follow",
+            referrer: "no-referrer",
+            body: JSON.stringify(
+                {
+                    message: err.message,
+                    stack: err.stack
+                }
+            )
+        }).then(response => response.json());
+    }
+
+    try {
+        setup_style();
+    } catch(err) {
+        log_error('');
+    }
+
+});
