2050acaddc43b599f5b56f9b2e4f5c1e71a067ad
saltb0rn <asche34@outlook.com>
2025-02-24 20:08:34 +0800

    update

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,117 +1,121 @@
 (function(){
-    let ptype;
-    let re_index = /^(?:\/|\/index\.html(?:\/|\/.*)?)$/;
-    let re_post = /^\/(?:.*)\.html(?:\/|\.*)?$/;
-    // the behaviors of different type of pages are different.
-    if (re_index.test(this.location.pathname)) {
-        ptype = 'index';
-    } else if (re_post.test(this.location.pathname)) {
-        ptype = 'post';
-    } else {
-        ptype = 'other';
+  let ptype;
+  let re_index = /^(?:\/|\/index\.html(?:\/|\/.*)?)$/;
+  let re_post = /^\/(?:.*)\.html(?:\/|\.*)?$/;
+  // the behaviors of different type of pages are different.
+  if (re_index.test(this.location.pathname)) {
+    ptype = 'index';
+  } else if (re_post.test(this.location.pathname)) {
+    ptype = 'post';
+  } else {
+    ptype = 'other';
+  }
+
+  function setup_style_for_index() {
+    console.log("The type of page is", ptype);
+    let validation = document.querySelector(".validation");
+    if (validation) {
+      validation.style.display = "none";
     }
+  }
 
-    function setup_style_for_index() {
-        console.log("The type of page is", ptype);
-        let validation = document.querySelector(".validation");
-        if (validation) {
-            validation.style.display = "none";
-        }
-    }
+  function setup_style_for_post() {
+    console.log("The type of page is", ptype);
+    let post_abstract = document.querySelector(".abstract");
+    let toc = document.querySelector("#table-of-contents");
 
-    function setup_style_for_post() {
-        console.log("The type of page is", ptype);
-        let post_abstract = document.querySelector(".abstract");
-        let toc = document.querySelector("#table-of-contents");
-        if (toc) {
-            post_abstract.parentNode.insertBefore(post_abstract, toc);
-            let isClosed = true;
-            toc.addEventListener('click', function() {
-                if (isClosed) {
-                    toc.classList.add('closed');
-                } else {
-                    toc.classList.remove('closed');
-                }
-                isClosed = !isClosed;
-            });
-        }
-        // TODO: 加载页面时移除评论块,之后设计一个按钮,点击的时候再把comment_clone添加回去.
-        // let comment = document.querySelector("#disqus_thread");
-        // let comment_clone = comment.cloneNode(true);
-        // if (comment) {
-        //     comment.style.display = "none";
-        //     comment.remove();
-        //     console.log(comment_clone);
-        // }
-    }
+    if (toc) {
+      post_abstract.parentNode.insertBefore(post_abstract, toc);
+      
+      const div = document.createElement("div");
+      div.classList.add("table-bookmark");
+      toc.appendChild(div);      
 
-    function setup_style_for_other() {
-        console.log("The type of page is", ptype);
-    }
-
-    let page_style_map = {
-        index: setup_style_for_index,
-        post: setup_style_for_post,
-        other: setup_style_for_other
-    };
-
-    function setup_style() {
-        let func = page_style_map[ptype];
-        if (func) {
-            func();
+      div.addEventListener('click', function(event) {
+        if (!toc.classList.contains("open")) {
+          toc.classList.add('open');
         } else {
-            console.log("No such type of page");
+          toc.classList.remove('open');
         }
+      });
     }
-
-    // function log_error(err, url='') {
-    //     if (!url) {
-    //         let div = (window.document.querySelector("div.errlogs") ||
-    //                    window.document.createElement('div'));
-    //         if (!("errlogs" in div.classList))
-    //         {
-    //             window.document.appendChild(div);
-    //             div.className = "errlogs";
-    //         }
-    //         let p = window.document.createElement('p');
-    //         p.className = 'err';
-    //         p.textContent = "message: "+ err.message + "\n" + "stack: " + er.stack + "\n";
-    //         div.appendChild(p);
-    //         return;
-    //     }
-    //     fetch(url, {
-    //         method: "POST",
-    //         mode: "no-cache",
-    //         credentials: "same-origin",
-    //         headers: {
-    //             "Content-Type": "application/json; charset=utf-8"
-    //         },
-    //         redirect: "follow",
-    //         referrer: "no-referrer",
-    //         body: JSON.stringify(
-    //             {
-    //                 message: err.message,
-    //                 stack: err.stack
-    //             }
-    //         )
-    //     }).then(response => response.json());
+    // TODO: 加载页面时移除评论块,之后设计一个按钮,点击的时候再把comment_clone添加回去.
+    // let comment = document.querySelector("#disqus_thread");
+    // let comment_clone = comment.cloneNode(true);
+    // if (comment) {
+    //     comment.style.display = "none";
+    //     comment.remove();
+    //     console.log(comment_clone);
     // }
+  }
 
-    try {
-        setup_style();
-    } catch(err) {
-        // log_error('');
-    }
+  function setup_style_for_other() {
+    console.log("The type of page is", ptype);
+  }
 
-    if ('serviceWorker' in navigator) {
-        // navigator.serviceWorker.register('/docs/sw.js');
-        navigator.serviceWorker.getRegistrations()
-            .then(regs => {
-                for (let reg of regs) {
-                    if (reg.scope.indexOf(location.origin) >= 0) {
-                        reg.unregister();
-                    }
-                }
-            });
+  let page_style_map = {
+    index: setup_style_for_index,
+    post: setup_style_for_post,
+    other: setup_style_for_other
+  };
+
+  function setup_style() {
+    let func = page_style_map[ptype];
+    if (func) {
+      func();
+    } else {
+      console.log("No such type of page");
     }
+  }
+
+  // function log_error(err, url='') {
+  //     if (!url) {
+  //         let div = (window.document.querySelector("div.errlogs") ||
+  //                    window.document.createElement('div'));
+  //         if (!("errlogs" in div.classList))
+  //         {
+  //             window.document.appendChild(div);
+  //             div.className = "errlogs";
+  //         }
+  //         let p = window.document.createElement('p');
+  //         p.className = 'err';
+  //         p.textContent = "message: "+ err.message + "\n" + "stack: " + er.stack + "\n";
+  //         div.appendChild(p);
+  //         return;
+  //     }
+  //     fetch(url, {
+  //         method: "POST",
+  //         mode: "no-cache",
+  //         credentials: "same-origin",
+  //         headers: {
+  //             "Content-Type": "application/json; charset=utf-8"
+  //         },
+  //         redirect: "follow",
+  //         referrer: "no-referrer",
+  //         body: JSON.stringify(
+  //             {
+  //                 message: err.message,
+  //                 stack: err.stack
+  //             }
+  //         )
+  //     }).then(response => response.json());
+  // }
+
+  try {
+    setup_style();
+  } catch(err) {
+    // log_error('');
+  }
+
+  if ('serviceWorker' in navigator) {
+    // navigator.serviceWorker.register('/docs/sw.js');
+    navigator.serviceWorker.getRegistrations()
+      .then(regs => {
+        for (let reg of regs) {
+          if (reg.scope.indexOf(location.origin) >= 0) {
+            reg.unregister();
+          }
+        }
+      });
+  }
 })();
